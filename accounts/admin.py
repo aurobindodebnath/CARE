11dcfa23bcf9e59a5d4e83859120319865123186
aurobindodebnath <aurobindodebnath@gmail.com>
2020-09-17 23:20:19 -0700

    Combining apps

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Organization, Department
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Organization)
-admin.site.register(Department)
--- activities/admin.py
+++ activities/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import ApplicationSecurity, VAPTAssessment, ConfigurationReview
+from .models import ApplicationSecurity, VAPTAssessment, ConfigurationReview, Organization, Department, UserProfile, AppSecComments, VAPTComments, ConfigComments
 # Register your models here.
 
 admin.site.register(ApplicationSecurity)
 admin.site.register(VAPTAssessment)
 admin.site.register(ConfigurationReview)
+admin.site.register(Organization)
+admin.site.register(Department)
+admin.site.register(UserProfile)
+admin.site.register(AppSecComments)
+admin.site.register(VAPTComments)
+admin.site.register(ConfigComments)
